4fa4a217e5381ec81ca171e2f899a45f591d82f8
Jimmy <piubitt@gmail.com>
2026-07-12 12:57:28 +0700

    c

--- src/components/sections/Countdown.tsx
+++
@@ -1,3 +0,0 @@
-export default function Countdown() {
-  return null;
-}
--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/**
- * Menggabungkan kelas Tailwind dengan cerdas menggunakan clsx dan tailwind-merge.
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
